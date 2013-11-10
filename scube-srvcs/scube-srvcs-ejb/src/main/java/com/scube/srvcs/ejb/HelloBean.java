5824ad521ee267e1e7dbefd39b5039db4c1f9e24
Sunil Hubert <sunilhubert@yahoo.com>
2013-11-10 16:41:18 +0530

    Committing HelloBean in scube-srvcs-ejb

---
+++ scube-srvcs/scube-srvcs-ejb/src/main/java/com/scube/srvcs/ejb/HelloBean.java
@@ -0,0 +1,20 @@
+package com.scube.srvcs.ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class HelloBean
+ */
+@Stateless
+@LocalBean
+public class HelloBean {
+
+    /**
+     * Default constructor. 
+     */
+    public HelloBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
